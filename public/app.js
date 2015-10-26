89ab9cd31252455dd44e73366d68203887623fb4
Metloff <genrih.metlov@gmail.com>
2015-10-26 13:54:02 +0300

    Updates orders input field

--- public/app.js
+++ public/app.js
@@ -19,8 +19,15 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
-	alert('Items in your cart: ' + cart_get_number_of_items());
 
+	update_orders_input()
+
+}
+
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
 }
 
 function cart_get_number_of_items()
@@ -39,5 +46,24 @@ function cart_get_number_of_items()
 	}
 	
 	return cnt;
+}
+
+
+function cart_get_orders()
+{
+	var orders = '';
+
+	for (var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0)
+		{	
+			orders = orders + key + "=" + value + ",";
+		}
+	}
+	
+	return orders;
 	
 }
