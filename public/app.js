646f628e9aa52f2a795ee786f5faed40280e09bd
Metloff <genrih.metlov@gmail.com>
2015-10-26 14:12:25 +0300

    Update orders button

--- public/app.js
+++ public/app.js
@@ -20,7 +20,8 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 
 
-	update_orders_input()
+	update_orders_input();
+	update_orders_button();
 
 }
 
@@ -28,6 +29,12 @@ function update_orders_input()
 {
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
 }
 
 function cart_get_number_of_items()
