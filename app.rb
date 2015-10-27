7dbb6ef55a2ecdb32e3dca085f8b32361aa504a3
Metloff <genrih.metlov@gmail.com>
2015-10-27 09:24:30 +0300

    Add order list on /cart

--- app.rb
+++ app.rb
@@ -20,6 +20,25 @@ get '/about' do
 end
 
 post '/cart' do
-	erb "Hello"
 
+	@hh = {}
+	@hh1 = {
+		'1' => "Hawaiian",
+		'2' => "Pepperoni",
+		'3' => "Vegeterian"
+	}
+
+	order = params[:orders]
+	s1 = order.split(/,/)
+	
+	s1.each do |x|
+		
+		s2 = x.split("=")
+		s3 = s2[0].split (/_/)
+
+		@hh[s3[1]] = s2[1]
+
+	end
+
+	erb :cart
 end
