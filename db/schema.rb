87df5a50df814d393a5fa587f14e8f02a776475a
Metloff <genrih.metlov@gmail.com>
2015-10-28 19:32:40 +0300

    Rename columns in order migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028072321) do
+ActiveRecord::Schema.define(version: 20151028100141) do
 
   create_table "orders", force: :cascade do |t|
-    t.text     "order"
-    t.text     "title"
+    t.text     "orders_input"
+    t.text     "name"
     t.text     "phone"
-    t.text     "adress"
+    t.text     "address"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
